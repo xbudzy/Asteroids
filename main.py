009b0dd52dba94098a5aebf8c3dda9662e27fe0d
xbudzy <jakub.a.budzynski@gmail.com>
2025-06-04 18:35:00 +0200

    a lot of updates

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     black = pygame.Color(0, 0, 0)
     print("Starting Asteroids!")
@@ -13,8 +17,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(black)
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,31 @@
+import pygame
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+        
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen, pygame.Color("White"), self.triangle(), 2)
+
+    def rotate(dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            # ?
+        if keys[pygame.K_d]:
+            # ?
+        
